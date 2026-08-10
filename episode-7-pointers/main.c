c2ad539a5ad1a4d6d25cb5bbb57d09a7280bf4df
learndataa <63557075+learndataa@users.noreply.github.com>
2026-08-10 14:57:42 +0530

    Refactor LED blinking to use pointers for delay
    Updated LED blinking logic to demonstrate pointer usage.

--- episode-7-pointers/main.c
+++ episode-7-pointers/main.c
@@ -32,21 +32,17 @@ int main(void)
 
   uint16_t delay_ms = 500;
 
-  try_by_value(delay_ms); // passes a COPY of delay_ms - the real one is untouched
-
   // Slow blink for a few seconds - proves try_by_value had no effect
-  for (uint8_t i = 0; i < 6; i++)
-  {
-    BSP_LED_Toggle();
-    HAL_Delay(delay_ms);
-  }
+  //try_by_value(delay_ms); // passes a COPY of delay_ms - the real one is untouched
 
-  try_by_pointer(&delay_ms); // passes the ADDRESS - the real delay_ms can be reached
+  // fast blink for the rest of time - proves try_by_pointer did change delay_ms
+  try_by_pointer(delay_ms); // passes the ADDRESS - the real delay_ms can be reached
 
-  // Fast blink forever - delay_ms was genuinely changed to 100
+  // LED blink
   while (1)
   {
     BSP_LED_Toggle();
     HAL_Delay(delay_ms);
   }
+    
 }
